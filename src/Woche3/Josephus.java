0073e1f145d83a9360fc8ed648aabcec2ef2f7da
Victor <kowavic@gmail.com>
2023-04-29 15:04:27 +0200

    finished übung 3

---
+++ src/Woche3/Josephus.java
@@ -0,0 +1,22 @@
+package Woche3;
+
+public class Josephus {
+
+    public static int loese(int n, int k) {
+        RDVL<Integer> rdvl = new RDVL<>();
+        rdvl.add(1);
+        for (int i = n; i > 1; i--) {
+            rdvl.add(i);
+        }
+
+        while (rdvl.size() > 1) {
+            rdvl.next(k-1);
+            rdvl.remove();
+        }
+        return rdvl.element();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(loese(1,1));
+    }
+}
